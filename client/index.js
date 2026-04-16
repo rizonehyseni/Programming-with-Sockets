22de5b02db561f7428ad2c4bf1f7071edc40368b
Rrezon Ibishi <rrezon.ibishi@gmail.com>
2026-04-16 20:33:07 +0200

    Trajtimi i pergjigjeve nga serveri

--- client/index.js
+++ client/index.js
@@ -17,3 +17,27 @@ function send(msgObj) {
   const buf = Buffer.from(JSON.stringify(msgObj));
   client.send(buf, SERVER_PORT, SERVER_IP);
 }
+
+
+client.on('message', (msg, rinfo) => {
+  try {
+    const res = JSON.parse(msg.toString());
+    console.log('\n=== Pergjigja nga Serveri ===');
+    if (res.status === 'ok') {
+      if (res.data && res.data.content) {
+        const savePath = path.join(process.cwd(), res.data.filename);
+        fs.writeFileSync(savePath, Buffer.from(res.data.content, 'base64'));
+        console.log(`File u shkarkua dhe u ruajt si: ${res.data.filename}`);
+      } else if (res.data) {
+        console.dir(res.data, { depth: null });
+      } else if (res.message) {
+        console.log(res.message);
+      }
+    } else {
+      console.log('Gabim:', res.message);
+    }
+  } catch (e) {
+    console.log('Mesazhi nuk eshte parsuar: ', msg.toString());
+  }
+  prompt();
+});
