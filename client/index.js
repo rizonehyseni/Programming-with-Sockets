6459f8d005f78bcca42076052785b5ac8083f369
Rrezon Ibishi <rrezon.ibishi@gmail.com>
2026-04-16 15:50:28 +0200

    krijo socket UDP dhe funksionin send

--- client/index.js
+++ client/index.js
@@ -5,3 +5,15 @@ const readline = require('readline');
 
 const SERVER_IP = process.argv[2] || '127.0.0.1';
 const SERVER_PORT = 41234;
+
+const client = dgram.createSocket('udp4');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function send(msgObj) {
+  const buf = Buffer.from(JSON.stringify(msgObj));
+  client.send(buf, SERVER_PORT, SERVER_IP);
+}
