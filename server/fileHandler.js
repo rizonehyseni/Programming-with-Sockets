6adacff4903159996e2c52c439c611ac16bb51b6
Rrezon Ibishi <rrezon.ibishi@gmail.com>
2026-04-17 18:20:00 +0200

    shto funksionin per listimin e skedareve

--- server/fileHandler.js
+++ server/fileHandler.js
@@ -3,3 +3,10 @@ const path = require('path');
 const config = require('./config');
 
 const filesDir = config.FILES_DIR;
+
+
+module.exports = {
+    listFiles() {
+        return fs.readdirSync(filesDir);
+    },
+};
