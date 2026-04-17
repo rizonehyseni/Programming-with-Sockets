b6547f94549d4b4eb6699d005dd46dfd9d2ea778
Rizona <rizonehyseni@gmail.com>
2026-04-17 17:08:39 +0200

    shtimi i t dhenave baze per klientin (roli, aktiviteti, numri i mesazheve)

--- server/clientManager.js
+++ server/clientManager.js
@@ -9,9 +9,19 @@ module.exports = {
     const key = `${rinfo.address}:${rinfo.port}`;
 
     if (!clients.has(key)) {
-      clients.set(key, {});
+      clients.set(key, {
+  role: config.NORMAL_ROLE,
+  lastActive: Date.now(),
+  messageCount: 0
+});
+
     }
 
     return true;
   }
 };
+
+
+
+
+
