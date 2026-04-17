3c1cb5b8160e17a9307763a8f7ed8290484f6ac2
Rrezon Ibishi <rrezon.ibishi@gmail.com>
2026-04-17 18:29:28 +0200

    shto funksionalitetin per fshirjen e skedareve

--- server/fileHandler.js
+++ server/fileHandler.js
@@ -9,4 +9,32 @@ module.exports = {
     listFiles() {
         return fs.readdirSync(filesDir);
     },
+
+    readFile(filename) {
+        const filePath = path.join(filesDir, filename);
+        if (fs.existsSync(filePath)) {
+            return fs.readFileSync(filePath, 'utf8');
+        }
+        throw new Error('File nuk u gjet');
+    },
+
+    deleteFile(filename) {
+        const filePath = path.join(filesDir, filename);
+        if (fs.existsSync(filePath)) {
+        fs.unlinkSync(filePath);
+            return true;
+        }
+        throw new Error('File nuk u gjet');
+    },
+
+    getFileInfo(filename) {
+        const filePath = path.join(filesDir, filename);
+        if (!fs.existsSync(filePath)) throw new Error('File nuk u gjet');
+        const stats = fs.statSync(filePath);
+        return {
+            size: stats.size,
+            created: stats.birthtime,
+            modified: stats.mtime
+    };
+  },
 };
