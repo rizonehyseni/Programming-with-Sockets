a0e93e474681cc3553f731b527e4d899448c88be
Rizona <rizonehyseni@gmail.com>
2026-04-18 23:05:54 +0200

    callback per trajtimin e gabimeve ne dergim

--- server/utils.js
+++ server/utils.js
@@ -3,5 +3,10 @@ module.exports = {
     return `${rinfo.address}:${rinfo.port}`;
   },
 
-  sendMessage(socket, rinfo, obj) {},
+  sendMessage(socket, rinfo, obj) {
+    const buf = Buffer.from(JSON.stringify(obj));
+    socket.send(buf, 0, buf.length, rinfo.port, rinfo.address, (err) => {
+      if (err) console.error("Gabim gjate dergimit:", err);
+    });
+  },
 };
