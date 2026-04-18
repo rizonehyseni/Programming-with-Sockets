021734d548521172c68ad1416d3bae12611d5b59
Rizona <rizonehyseni@gmail.com>
2026-04-18 10:33:31 +0200

    krijova objektin baze response

--- server/commandHandler.js
+++ server/commandHandler.js
@@ -4,6 +4,12 @@ const clientManager = require('./clientManager');
 module.exports = {
   handle(msg, clientKey) {
     const isAdmin = clientManager.isAdmin(clientKey);
+    const response = { 
+       type: 'response', 
+       status: 'ok',
+       isAdmin: isAdmin 
+};
+  
 
   }
 };
