afddd8c72a1203155fd6f41bc6f3304c3cf32522
Rrezon Ibishi <rrezon.ibishi@gmail.com>
2026-04-19 13:54:27 +0200

    shto route / stats me te dhenat baze

--- server/httpServer.js
+++ server/httpServer.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const config = require('./config');
+const clientManager = require('./clientManager');
 const app = express();
 
 module.exports = {
   start() {
+    app.get('/stats', (req, res) => {
+        const stats = {
+            status: "ok",
+            koha: new Date().toISOString()
+        };
+        res.json(stats);
+    });
     app.listen(config.HTTP_PORT, () => {
       console.log(`Serveri HTTP per monitorim eshte aktiv ne: http://localhost:${config.HTTP_PORT}/stats`);
     });
